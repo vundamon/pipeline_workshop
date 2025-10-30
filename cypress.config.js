1c8527d011295c943df6717346f7dd415eb9af05
Balazs Hosszu <balazs.hosszu@adnovum.hu>
2025-10-30 10:21:52 +0100

    Cypress should report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'cypress';
+import mochawesome from 'cypress-mochawesome-reporter/plugin.js';
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'build/cypress',
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveJson: true,
+  },
   e2e: {
     baseUrl: 'http://localhost:4200',
-    supportFile: false,
+    setupNodeEvents(on, config) {
+      mochawesome(on);
+      return config;
+    },
+    supportFile: 'cypress/support/e2e.ts',
   },
   video: false,
 });
---
+++ cypress/support/e2e.ts
@@ -0,0 +1,3 @@
+import 'cypress-mochawesome-reporter/register';
+
+// You can add global Cypress configuration or custom commands here.
